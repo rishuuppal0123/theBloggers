a97f144cc272741710d0e34ba0c30bcac911227e
rishav.uppal <rishav.uppal@paytail.com>
2024-03-02 01:12:52 +0530

    changed end point

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
         { currentUser?.id && isNavShowing  && <ul className="nav__menu">
           <li><Link to = {`/profile/${currentUser.id}`} onClick={closeNavHandler}>{currentUser?.name}</Link></li>
           <li><Link to = "/create" onClick={closeNavHandler}>Create Post</Link></li>
-          <li><Link to = "/users" onClick={closeNavHandler}>Authors</Link></li>
+          <li><Link to = "/authors" onClick={closeNavHandler}>Authors</Link></li>
           <li><Link to = "/logout" onClick={closeNavHandler}>Logout</Link></li>
         </ul>}
         { !currentUser?.id && isNavShowing  && <ul className="nav__menu">
